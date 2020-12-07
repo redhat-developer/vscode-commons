80fe3a1485406eecd5ceda679b15a1ea96c47524
Shubham Mathur <shumathu@redhat.com>
2020-12-07 23:10:46 +0530

    set limit to queue

--- src/utils/telemetryEventQueue.ts
+++ src/utils/telemetryEventQueue.ts
@@ -2,11 +2,13 @@ import { TelemetryEvent } from '../interfaces/telemetryEvent';
 
 let queue: TelemetryEvent[] | undefined;
 export namespace TelemetryEventQueue {
-  const MAX_QUEUE_SIZE = 5;
+  const MAX_QUEUE_SIZE = 35;
   export function initialize() {
     queue = [];
   }
-
+  /*
+    split() should work fine until we choose to have high MAX_QUEUE_SIZE
+   */
   export function addEvent(e: TelemetryEvent) {
     if (queue?.length === MAX_QUEUE_SIZE) {
       queue?.shift();
